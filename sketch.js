ca9e5a0e088447aa7a02c8cbea87cf6e97dd674a
KanishkCoder <76817700+KanishkCoder@users.noreply.github.com>
2021-01-12 07:13:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,67 @@
-var fixedRect, movingRect;
+var cat,cat_running;
+var mouse,mouse2Img;
+var background,backgroundImg;
+var catImg,mouseImg;
 
-function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
+function preload() {
+    //load the images here
+    catImg=loadImage("images/tomFour.png");
+    cat_running=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    mouseImg=loadImage("images/jerryOne.png");
+    mouse2Img=loadImage("images/jerryFour.png")
+    backgroundImg=loadImage("images/garden.png");
+}
 
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    cat=createSprite(800,650,30,30);
+    cat.addAnimation("cat_running");
+    cat.scale=0.2;
+
+    mouse=createSprite(200,650,10,10);
+    mouse.addAnimation(mouseImg);
+    mouse.scale=0.1;
+
 }
 
 function draw() {
-  background(0,0,0);  
 
-  bounceOff(movingRect,fixedRect)
+    background(backgroundImg);
+    
+    cat.x=World.mouseX;
+    
 
-  drawSprites();
+    //Write condition here to evalute if tom and jerry collide
+    
+    
+
+    if(mouse.x-cat.x<1000){
+        mouse.addAnimation("mouse2Img",mouseImg)
+        mouse.changeAnimation("mouse2Img")
+    }
+    
+    if(cat.velocityX=-5){
+        cat.addAnimation("catImg",cat_running)
+        cat.changeAnimation("cat_running");
+    }
+    if(cat.isTouching(mouse)){
+        cat.velocityX=0;
+    }
+    
+    drawSprites();
+
+    keyPressed();
 
 }
 
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+  if(keyWentDown===LEFT_ARROW){
+   cat.velocityX=-5;
+   cat.addAnimation("cat_running",catImg);
+   cat.changeAnimation("cat_running");
+  }
+}
